33d61b9e229ca77dab08bf591bcd110c412146c2
Sourav Nayek <nayek.sourav18@gmail.com>
2023-06-15 12:48:16 +0530

    All Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.scss';
+
+import loader from '../src/assets/loder.gif';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=1');
+      setUsers(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <span className="brand">Tech Sourav</span>
+        <button className="btn" onClick={getUsers} disabled={isLoading}>
+          {isLoading ? (
+            <div className="loader-container">
+              <img src={loader} alt="Loading..." className="loader" style={{ width: '50px', height: '50px' }} />
+            </div>
+          ) : (
+            'Get Users'
+          )}
+        </button>
+      </nav>
+
+      <div className="user-grid">
+        {users.map((user) => (
+          <div className="user-card" key={user.id}>
+            <img src={user.avatar} alt={user.first_name} />
+            <h3>{`${user.first_name} ${user.last_name}`}</h3>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
